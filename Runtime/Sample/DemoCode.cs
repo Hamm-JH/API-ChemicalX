8acfe8285cd81fd75fd56a224d06e76dde730f95
Hamm-JH <rimmoyee@gmail.com>
2022-09-19 23:52:22 +0900

    0.1.12 Update DemoCode

--- Runtime/Sample/DemoCode.cs
+++ Runtime/Sample/DemoCode.cs
@@ -11,33 +11,52 @@ namespace ChemicalX.Core
 {
     public partial class DemoCode : MonoBehaviour
     {
+        public bool bDebugEEG;
+        public bool bDebugStrongestEEGPart;
+        public bool bDebugDeviceNoises;
+        public bool bDebugIsDeviceEquipped;
+
         private void Start()
         {
             // 빌드 타입 디버그로 설정
             GlobalSetting.BUILDTYPE = BuildType.Debug;
 
+            // 데모용 테스트 데이터 생성코드 실행
             DemoRunner.Instance.Run();
         }
 
         private void OnDestroy()
         {
+            // 종료시 비동기 코드 동작 정지
             DemoRunner.Instance.Destroy();
         }
 
         // Update is called once per frame
         void Update()
         {
-            // 1. EEG 데이터 출력
-            PrintEEG(BrainAPI.CurrentEEG);
+            if (bDebugEEG)
+            {
+                // 1. EEG 데이터 출력
+                PrintEEG(BrainAPI.CurrentEEG);
+            }
 
-            // 2. Strongest EEG part 데이터 출력
-            PrintStrongestEEGPart(BrainAPI.StrongestEEGPart);
+            if (bDebugStrongestEEGPart)
+            {
+                // 2. Strongest EEG part 데이터 출력
+                PrintStrongestEEGPart(BrainAPI.StrongestEEGPart);
+            }
 
-            // 3. Device noises 출력
-            PrintDeviceNoises(BrainAPI.DeviceNoises);
+            if (bDebugDeviceNoises)
+            {
+                // 3. Device noises 출력
+                PrintDeviceNoises(BrainAPI.DeviceNoises);
+            }
 
-            // 4. Equip status 출력
-            PrintEquipStatus(BrainAPI.IsDeciveEquipped);
+            if (bDebugIsDeviceEquipped)
+            {
+                // 4. Equip status 출력
+                PrintEquipStatus(BrainAPI.IsDeciveEquipped);
+            }
         }
     }
 }
